b396b4c180cce8b72a7d228a113be4b20c1b087d
tom <root>
2017-08-10 15:24:08 -0700

    uploading banking console application

---
+++ src/banksystemspring2017/BankSystemSpring2017.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banksystemspring2017;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Tatha
+ */
+public class BankSystemSpring2017 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static ArrayList<BankAccount> allBankAccounts;
+    public static ArrayList<OnlineAccount> allOnlineAccoutns;
+    public static void main(String[] args) {
+        // TODO code application logic here
+        allBankAccounts = new <BankAccount>();
+        
+    }
+    
+}
